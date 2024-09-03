92c65b4c33685f9fe32aaa13d1423b38bbb52518
leokozan <113151772+leokozan@users.noreply.github.com>
2024-09-03 00:54:21 -0300

    First commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,64 +1,108 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
+import { ResizeMode, Video } from 'expo-av';
+import React, { useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Camera, useCameraDevice, useCameraPermission,
+  useMicrophonePermission } from 'react-native-vision-camera'
+import * as MediaLibrary from 'expo-media-library';
 export default function HomeScreen() {
+  const device = useCameraDevice('back');
+  const { hasPermission, requestPermission } = useCameraPermission()
+  const {hasPermission:hasMicPermission,requestPermission:requestMicPermission} = useMicrophonePermission();
+  const [permission,setPermission] = useState(Boolean);
+  
+  useEffect(()=>{
+    (async()=>{
+      const status = await requestPermission();
+      const statusMic = await requestMicPermission();
+      if(status && statusMic){
+        setPermission(true);
+      }
+      const {status:statusMediaLibrary} = await MediaLibrary.requestPermissionsAsync();
+      if(statusMediaLibrary!=='granted') return;
+    })
+  },[])
+  const startRecording = () => {
+    if (!cameraRef.current || !device) return;
+    setIsRecording(true);
+  
+    cameraRef.current.startRecording({
+      onRecordingFinished: (video) => {
+        setIsRecording(false);
+        setVideoUri(video.path);
+        setModalVisible(true);
+      },
+      onRecordingError: (error) => {
+        console.log(error);
+      }
+    });
+  };
+  
+  const stopRecording = async () => {
+    if(cameraRef.current){
+      await cameraRef.current.stopRecording();
+      setIsRecording(false);
+    }
+  }
+  const handleSaveVideo = async () =<{
+    if(videoUri){
+      try{
+        await MediaLibrary.createAssetAsync(videoUri);
+      }catch(error){
+        console.log(error)
+      }
+    }
+  }
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+    />
+    <TouchableOpacity
+    onPressIn={startRecording}
+    onPressOut={stopRecording}
+    />
+    <Video
+      source={{
+        uri: videoUri
+      }}
+      rate={1.0}
+      volume={1.0}
+      isMuted={false}
+      shouldPlay
+      isLooping
+      resizeMode={ResizeMode.COVER}
+      style={{ width: 300, height: 300 }}
+    />
+
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+  },
   titleContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
   stepContainer: {
     gap: 8,
     marginBottom: 8,
+  },
+  step: {
+    fontSize: 16,
   },
   reactLogo: {
     height: 178,
